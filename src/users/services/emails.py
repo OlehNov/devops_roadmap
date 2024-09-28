456f52f24289754cd6f3cef8d3ccadead9bb3ad7
vsem <virtualik@gmail.com>
2024-09-28 15:29:16 +0200

    load dotenv fixed

--- src/users/services/emails.py
+++ src/users/services/emails.py
@@ -6,9 +6,12 @@ from django.core.mail import EmailMessage, send_mail
 from django.template.loader import render_to_string
 from django.utils.encoding import force_bytes
 from django.utils.http import urlsafe_base64_encode
+from dotenv import load_dotenv
 
 from users.utils import TokenGenerator
 
+
+load_dotenv()
 
 User = get_user_model()
 
