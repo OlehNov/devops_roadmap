5104ae3a5bc00d8e7a39103eada49750b5159579
franzferdinnand <serhiyturchyn@gmail.com>
2024-10-07 09:03:13 +0200

    ping-pong view changed to a function

--- src/config/api.py
+++ src/config/api.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 
 
 def ping_pong_view(request):
-    if request.method == "GET":
+    if request.method.upper() == "GET":
         return HttpResponse("{'ping': 'pong'}")
     else:
         return HttpResponse("POST, PUT, PATCH methods are not allowed")
