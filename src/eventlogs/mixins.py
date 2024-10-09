d0a79dac0d6ff71790ac31e99a2436aec4de208c
franzferdinnand <serhiyturchyn@gmail.com>
2024-10-09 10:43:01 +0200

    docstrings written on EventLogMixin

--- src/eventlogs/mixins.py
+++ src/eventlogs/mixins.py
@@ -5,7 +5,17 @@ from eventlogs.models import EventLog
 
 
 class EventLogMixin:
+    """
+    Using for logging Create, Update, Delete events
 
+    Implement by inheriting from EventLogMixin and using log_event method,
+    must be implemented after successfully setting changes to main Database
+
+    log_event method takes two arguments: request and object, on which was the operation made
+    by checking request method - defining which operation was made and set operation_type value when calling _write_to_db method, which makes new DB raw
+
+    on database, it writes the ID of user, email of user, object on which operation was made, type of the operation and timestamp
+    """
 
     def _write_to_db(request, operation_type, operated_object):
         EventLog.objects.create(
