1ae09330eecb5be7341fbaad5c0c3779053ce1c5
franzferdinnand <serhiyturchyn@gmail.com>
2024-10-20 07:31:39 +0200

    logout endpoint added

--- src/authentication/api.py
+++ src/authentication/api.py
@@ -1,3 +1,8 @@
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -21,3 +26,17 @@ class CustomTokenRefreshView(TokenRefreshView):
 
 class CustomTokenVerifyView(TokenVerifyView):
     serializer_class = CustomTokenVerifySerializer
+
+
+class UserLogoutView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        try:
+            refresh_token = request.data["refresh_token"]
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+
+            return Response({"message": "Successfully logged out."}, status=status.HTTP_200_OK)
+        except Exception as e:
+            return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
--- src/authentication/urls.py
+++ src/authentication/urls.py
@@ -3,11 +3,12 @@ from django.urls import path
 from authentication.api import (
     CustomObtainTokenPairView,
     CustomTokenRefreshView,
-    CustomTokenVerifyView,
+    CustomTokenVerifyView, UserLogoutView,
 )
 
 urlpatterns = [
     path("token/", CustomObtainTokenPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", CustomTokenRefreshView.as_view(), name="token_refresh"),
     path("token/verify/", CustomTokenVerifyView.as_view(), name="token_verify"),
+    path("logout/", UserLogoutView.as_view(), name="logout"),
 ]
