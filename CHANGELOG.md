16a269e8090aa4b1c6855547bd6cc86f1d30e18d
vsem <virtualik@gmail.com>
2024-10-27 02:29:32 +0100

    Wash The Cat. Part 3. Fixes. CHANGELOG.md added

---
+++ CHANGELOG.md
@@ -0,0 +1,13 @@
+# CHANGELOG
+
+---
+
+## 27.10.2024 03:16. Wash the Cat. Part 3
+
+1. Addons added.
+2. All features that are used in more than one app have been moved to addons.
+3. Categories api endpoints added.
+4. Washed the Cat))).
+5. CHANGELOG.md added
+
+---
--- src/users/utils.py
+++ src/users/utils.py
@@ -1,10 +1,6 @@
 from django.contrib.auth.tokens import PasswordResetTokenGenerator
 from six import text_type
 
-from administrators.models import Administrator
-from roles.constants import Role
-from tourists.models import Tourist
-
 
 class TokenGenerator(PasswordResetTokenGenerator):
     def _make_hash_value(self, user, timestamp):
