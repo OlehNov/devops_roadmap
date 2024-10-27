0e6d1410739985f0629286235115ba007f05bff3
franzferdinnand <serhiyturchyn@gmail.com>
2024-10-27 14:12:40 +0100

    Response fixed

--- src/addons/paginators/custom_list_view_paginator.py
+++ src/addons/paginators/custom_list_view_paginator.py
@@ -1,25 +1,27 @@
 import os
+from collections import OrderedDict
+
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 
-from config import settings
+from django.conf import settings
 
 
 class CustomListViewPageNumberPagination(PageNumberPagination):
 
     def get_page_size(self, request):
-        page_size = request.query_params.get("page_size", self.page_size)
+        page_size = request.query_params.get("pageSize", self.page_size)
         max_page_size = settings.MAX_PAGE_SIZE
         return min(int(page_size), max_page_size)
 
-    def get_paginated_response(self, data):
-        return Response({
-            "links": {
-                "next": self.get_next_link(),
-                "previous": self.get_previous_link()
-            },
-            "total": self.page.paginator.count,
-            "page": self.page.number,
-            "page_size": self.get_page_size(self.request),
-            "results": data
-        })
+    def get_paginated_response (self, data):
+        return Response(
+            OrderedDict([
+                ("count", self.page.paginator.count),
+                ("next", self.get_next_link()),
+                ("previous", self.get_previous_link()),
+                ("page", self.page.number),
+                ("pageSize", self.get_page_size(self.request)),
+                ("results", data),
+            ])
+        )
