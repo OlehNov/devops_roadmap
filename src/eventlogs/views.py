e2482d3684511d0de4477602b133feee7dfa6dbe
vsem <virtualik@gmail.com>
2024-10-27 03:19:53 +0100

    eventlog fixed

--- src/eventlogs/views.py
+++ src/eventlogs/views.py
@@ -1,6 +1,6 @@
 from rest_framework.generics import ListAPIView, RetrieveAPIView
 from rest_framework.pagination import PageNumberPagination
-from rest_framework.permissions import IsAdminUser
+from rest_framework.permissions import IsAdminUser, AllowAny
 from drf_spectacular.utils import extend_schema
 
 from eventlogs.models import EventLog
@@ -18,16 +18,6 @@ class EventLogListAPIView(ListAPIView):
 @extend_schema(tags=["eventlog"])
 class EventLogRetrieveAPIView(RetrieveAPIView):
     permission_classes = [IsAdminUser]
+    queryset = EventLog.objects.all()
     serializer_class = EventLogSerializer
     lookup_url_kwarg = "event_id"
-
-    def get_queryset(self):
-        user = self.request.user
-
-        if user.is_anonymous:
-            return EventLog.objects.none()
-
-        if user.is_staff and user.is_superuser:
-            return EventLog.objects.get(id=self.kwargs.get("event_id"))
-
-        return EventLog.objects.none()
