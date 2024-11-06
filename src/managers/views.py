bd1f6d0c1a5c9a8b0e0faef879c57cb6c9fd9a19
DmitryTok <FoB09@ukr.net>
2024-11-06 11:55:15 +0100

    changes on commit e187ded

--- src/managers/views.py
+++ src/managers/views.py
@@ -23,16 +23,18 @@ class ManagerModelViewSet(ModelViewSet):
         return ManagerSerializer
 
     def update(self, request, *args, **kwargs):
-        partial = kwargs.get("partial", False)
+        partial = kwargs.pop("partial", False)
         instance = self.get_object()
         serializer = self.get_serializer(
             instance, data=request.data, partial=partial
         )
-        serializer.is_valid(raise_exception=True)
-
-        self.perform_update(serializer)
-
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        if serializer.is_valid():
+            self.perform_update(serializer)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response(
+                serializer.errors, status=status.HTTP_400_BAD_REQUEST
+            )
 
     def partial_update(self, request, *args, **kwargs):
         kwargs["partial"] = True
