e3f6adb15b4d475138996650a24813739639b6b1
Oleksandr Pinchuk <@foxstudent106121>
2024-12-22 18:08:07 +0200

    adaptation event_log to serializers

--- src/addons/mixins/eventlog.py
+++ src/addons/mixins/eventlog.py
@@ -1,3 +1,5 @@
+from django.forms import model_to_dict
+
 from eventlogs.constants import OperationType
 from eventlogs.models import EventLog
 
@@ -43,7 +45,7 @@ class EventLogMixin:
             return ValueError(f"{value} is None")
         return value
 
-    def log_event(self, request, operated_object):
+    def log_event(self, request, operated_object, operation_type=None):
         """
         This method is used for logging event and must be implemented in your code after successfully writing to DB
         Takes two arguments - request and operated_object (object)
@@ -58,15 +60,67 @@ class EventLogMixin:
         }
 
         # checking request method and setting operation type depending on it
-        match request.method.upper():
-            case "POST":
-                operation_type = OperationType.CREATE
-            case "PUT" | "PATCH":
-                operation_type = OperationType.UPDATE
-            case "DELETE":
-                operation_type = OperationType.DELETE
-            case _:
-                raise ValueError(f"Unsupported HTTP method: {request.method}")
+        if not operation_type:
+            match request.method.upper():
+                case "POST":
+                    operation_type = OperationType.CREATE
+                case "PUT" | "PATCH":
+                    operation_type = OperationType.UPDATE
+                case "DELETE":
+                    operation_type = OperationType.DELETE
+                case _:
+                    raise ValueError(f"Unsupported HTTP method: {request.method}")
 
         # writing to Database
+        self._write_to_db(request, operation_type, instance)
+
+
+class EventLogForSerializersMixin:
+
+    def _write_to_db(self, request, operation_type, operated_object):
+        # checking user for authentication and setting variables depending on it
+        if request.user.is_authenticated:
+            user_id = request.user.id
+            user_email = request.user.email
+        else:
+            user_id = None
+            user_email = "Anonymous"
+
+        # creating a new row in database
+        EventLog.objects.create(
+            user_id=user_id,
+            user_email=user_email,
+            instance=operated_object,
+            operation_type=operation_type,
+        )
+
+    # validation for value is not None
+    def _validate(self, value):
+        if value is None:
+            return ValueError(f"{value} is None")
+        return value
+
+    def log_event_for_serializer(self, request, validated_data, operation_type=None):
+        """
+        Logs an event from serializers.
+        """
+        request = self._validate(request)
+
+        if validated_data is None:
+            raise ValueError("The serializer does not contain an instance for logging.")
+
+        instance = {
+            "instance_id": validated_data.id,
+            "instance_class": validated_data.__class__.__name__,
+            "validated_data": model_to_dict(validated_data),
+        }
+
+        if not operation_type:
+            if request.method == "POST":
+                operation_type = OperationType.CREATE
+            elif request.method in ["PUT", "PATCH"]:
+                operation_type = OperationType.UPDATE
+            else:
+                raise ValueError(f"Unsupported HTTP method: {request.method}")
+
         self._write_to_db(request, operation_type, instance)
--- src/categories/serializers.py
+++ src/categories/serializers.py
@@ -3,12 +3,13 @@ from rest_framework.serializers import ModelSerializer, ValidationError
 from slugify import slugify
 from unidecode import unidecode
 
+from addons.mixins.eventlog import EventLogForSerializersMixin
 from categories.models import Category
 from categories.validators import (validate_name_category,
                                    validate_slug_category)
 
 
-class CategorySerializer(ModelSerializer):
+class CategorySerializer(ModelSerializer, EventLogForSerializersMixin):
     name = serializers.CharField(max_length=120, required=True, validators=[validate_name_category])
     slug = serializers.SlugField(max_length=120, required=False, validators=[validate_slug_category])
 
@@ -37,3 +38,19 @@ class CategorySerializer(ModelSerializer):
             )
 
         return attrs
+
+    def create(self, validated_data):
+        category = super().create(validated_data)
+        request = self.context.get("request")
+
+        if request:
+            self.log_event_for_serializer(request=request, validated_data=category)
+        return category
+
+    def update(self, instance, validated_data):
+        updated_instance = super().update(instance, validated_data)
+        request = self.context.get("request")
+
+        if request:
+            self.log_event_for_serializer(request=request, validated_data=updated_instance)
+        return updated_instance
