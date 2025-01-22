3e1fd5c3f7e7adc1c1b6bff27d243f761c7bd100
DmitryTok <FoB09@ukr.net>
2025-01-22 15:14:08 +0100

    add check user_instance in tourists/services/emails.py, glamp_owners/services/owner_verify.py

--- src/glamp_owners/services/owner_verify.py
+++ src/glamp_owners/services/owner_verify.py
@@ -1,16 +1,23 @@
-import os
-
 from django.contrib.auth import get_user_model
 from django.core.mail import EmailMessage
 from django.http import HttpRequest
 from django.template.loader import render_to_string
+from rest_framework.response import Response
+from rest_framework import status
 from rest_framework_simplejwt.tokens import RefreshToken
 
 User = get_user_model()
 
 
-def send_verification_email(request: HttpRequest, user_id: int) -> None:
+def send_verification_email(request: HttpRequest, user_id: int) -> Response | None:
     user_instance = User.objects.get(id=user_id)
+
+    if not user_instance:
+        return Response(
+            {"detail": "Not Found"},
+            status=status.HTTP_404_NOT_FOUND,
+        )
+
     token = RefreshToken.for_user(user_instance)
     host = request.get_host()
     protocol = "https" if request.is_secure() else "http"
--- src/tourists/services/emails.py
+++ src/tourists/services/emails.py
@@ -1,14 +1,25 @@
 from django.contrib.auth import get_user_model
 from django.core.mail import EmailMessage
 from django.http import HttpRequest
+from rest_framework import status
+
 from django.template.loader import render_to_string
 from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.response import Response
+
 
 User = get_user_model()
 
 
-def send_activation_email(request: HttpRequest, user_id: int) -> None:
+def send_activation_email(request: HttpRequest, user_id: int) -> Response | None:
     user_instance = User.objects.get(id=user_id)
+
+    if not user_instance:
+        return Response(
+            {"detail": "Not Found"},
+            status=status.HTTP_404_NOT_FOUND,
+        )
+
     token = RefreshToken.for_user(user_instance)
     host = request.get_host()
     protocol = "https" if request.is_secure() else "http"
