24ff726cf10f75c1cda5c8da2517b35c83ed3fe7
z200801 <94222461+z200801@users.noreply.github.com>
2025-02-05 22:26:32 +0200

    Update create_vm.sh

--- create_vm.sh
+++ create_vm.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
-if [[ -z "$1" || -z "$2" || -z "$3" ]]; then
+if [ ?{#} -lt 3 ]; then 
+#if [[ -z "$1" || -z "$2" || -z "$3" ]]; then
     echo "Error: Missing required arguments."
     echo "Usage: ./script.sh <VM_NAME> <ISO_PATH> <HOME_PATH> [<VM_MEMORY> <VM_CPUS> <VM_DISK_SIZE>]"
     exit 1
 fi
 
-if [[ "$1" =~ ^[0-9]+$ || "$2" =~ ^[0-9]+$ || "$3" =~ ^[0-9]+$ ]]; then
-    echo "Error: VM_NAME, ISO_PATH, and HOME_PATH must be strings, not numbers."
-    exit 1
-fi
+#if [[ "$1" =~ ^[0-9]+$ || "$2" =~ ^[0-9]+$ || "$3" =~ ^[0-9]+$ ]]; then
+#    echo "Error: VM_NAME, ISO_PATH, and HOME_PATH must be strings, not numbers."
+#    exit 1
+#fi
 
 function _error(){
-      echo "Error: $1"
+      echo "Error: ${1}"
       exit 1
 }
 
@@ -24,18 +25,21 @@ VM_CPUS="$5"
 VM_DISK_SIZE="$6"
 
 
-if [ -z "$VM_NAME" ]; then _error "VM_NAME is required"; fi
-if [ -z "$ISO_PATH" ]; then _error "ISO_PATH is required"; fi
-if [ -z "$HOME_PATH" ]; then _error "HOME_PATH is required"; fi
+# Цей блок ти вже провірив 
+#if [ -z "$VM_NAME" ]; then _error "VM_NAME is required"; fi
+#if [ -z "$ISO_PATH" ]; then _error "ISO_PATH is required"; fi
+#if [ -z "$HOME_PATH" ]; then _error "HOME_PATH is required"; fi
+
 if [ -z "$VM_MEMORY" ]; then VM_MEMORY=1024; fi
 if [ -z "$VM_CPUS" ]; then VM_CPUS=1; fi
 if [ -z "$VM_DISK_SIZE" ]; then VM_DISK_SIZE=6000; fi
 
-
+# Зроби окрему папку для дисків віртуальних машин
 VM_DISK="${HOME_PATH}/${VM_NAME}.vdi"
+
 BRIDGE_ADAPTER=$(ip -br r sh default | awk '{print $5}')
 
-
+# --ostype Ubuntu_64 - винеси в змінну
 if ! VBoxManage createvm --name "${VM_NAME}" --ostype Ubuntu_64 --register; then _error "Failed to create VM"; fi
 
 if ! VBoxManage modifyvm "${VM_NAME}" --memory "${VM_MEMORY}" --cpus "${VM_CPUS}" --nic1 bridged --bridgeadapter1 "${BRIDGE_ADAPTER}"; then _error "Failed to modify VM"; fi
